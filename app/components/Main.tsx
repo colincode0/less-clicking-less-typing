1050df4b2dffb009be6fbd98c9efe20ec2866b8f
colindharrington <colindavidharrington@gmail.com>
2024-04-18 16:22:53 -0700

    proper router

--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -1,12 +1,24 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
+import { useSearchParams } from "next/navigation";
 
 export default function Main() {
-  const [input, setInput] = React.useState("");
+  const searchParams = useSearchParams();
+  const buttons = searchParams.get("buttons");
+
+  console.log(buttons);
+
+  const [input, setInput] = useState("");
 
   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
     setInput(e.target.value);
   }
+
+  function handleClick() {
+    const url = `https://stockcharts.com/sc3/ui/?s=${input}`;
+    window.open(url, "_blank");
+  }
+
   return (
     <div>
       Main
@@ -17,7 +29,9 @@ export default function Main() {
         value={input}
         onChange={handleChange}
       />
-      <button className="btn btn-primary">Link</button>
+      <button onClick={handleClick} className="btn btn-primary">
+        Go to Link
+      </button>
     </div>
   );
 }
