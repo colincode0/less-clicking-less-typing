0ab98fcbf9e6e8d6630fc463ebc9b51a95738785
colindharrington <colindavidharrington@gmail.com>
2024-04-19 13:05:03 -0700

    centered styling

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,11 +1,12 @@
 import Main from "@/components/Main";
 import Image from "next/image";
 import { Suspense } from "react";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+      <div className="font-mono text-sm">
         <Suspense>
           <Main />
         </Suspense>
